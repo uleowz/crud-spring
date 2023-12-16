10577083e15b577c432af8a441b28129df524423
Leonardo Azzola <uleowz@gmail.com>
2023-12-16 09:29:49 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)

--- src/main/java/com/uleowz/crudspring/controller/CourseController.java
+++ src/main/java/com/uleowz/crudspring/controller/CourseController.java
@@ -1,5 +1,8 @@
 package com.uleowz.crudspring.controller;
 
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,13 @@ import com.uleowz.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController // avisando ao spring que esta Classe terá endpoints (url)
 @RequestMapping("/api/courses") // dizendo qual é o endereço(url) que ira acessar esta classe 
@@ -23,6 +33,15 @@ public class CourseController {
     @GetMapping // método deve ser acionado quando chegar uma requisição HTTP GET que corresponde ao URL.
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.POST);
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 
     
