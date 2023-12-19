ea4aa04365ec30bece6a4a23075b916faf176128
Leonardo Azzola <uleowz@gmail.com>
2023-12-19 15:15:04 -0300

    Remover Curso: API Spring HTTP DELETE

--- src/main/java/com/uleowz/crudspring/controller/CourseController.java
+++ src/main/java/com/uleowz/crudspring/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.uleowz.crudspring.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,6 +66,13 @@ public class CourseController {
         }).orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+        return courseRepository.findById(id).map(recordFound -> {
+            courseRepository.deleteById(id);
+            return ResponseEntity.noContent().<Void>build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
     
 
 }
